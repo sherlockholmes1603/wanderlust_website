77a36d3c368b97c655eef3d76324a9b6e6c297a5
sherlockholmes1603 <chahak.jethani.eee20@itbhu.ac.in>
2024-05-13 12:27:28 +0530

    GeoCoding and Map Display Completed

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,16 @@
-mapboxgl.accessToken =
-  window.mapToken;
+mapboxgl.accessToken = mapToken;
 
-console.log(window.mapToken);
-console.log(window.coordinates);
+console.log(mapToken);
+console.log(coordinates);
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v12",
   // projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
   zoom: 9,
-  center: window.coordinates,
+  center: coordinates,
 });
 
-const marker = new mapboxgl.Marker()
-  .setLngLat(window.coordinates)
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(coordinates)
+  .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML("<p>This is the exact location of the hotel</p>"))
   .addTo(map);
